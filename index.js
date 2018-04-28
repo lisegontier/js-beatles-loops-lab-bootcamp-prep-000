9ef5850989ffc33f05abe0a93631cebdfdb8fb11
Liz Gunter <midwestunicorn@gmail.com>
2018-04-28 20:14:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo St
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 function theBeatlesPlay(musicians, instruments) {
-  var theBeatlesPlay = new Array(theBeatlesPlay);
+  var array = [];
   for (let i = 0; i > 4; i++) {
-    console.log('${musicians} + "plays" + ${instruments}');
+    array.push('${musicians} + "plays" + ${instruments}');
   }
   return array;
 }
