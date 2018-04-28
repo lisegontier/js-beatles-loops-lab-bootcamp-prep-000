461a75f052d014d7cdc317273fa03b52a025ca4d
Liz Gunter <midwestunicorn@gmail.com>
2018-04-28 20:33:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 
-var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 function theBeatlesPlay(musicians, instruments) {
   const array = [];
   
   for (let i = 0, l = musicians.length; i < l; i++) {
-    array.push('${musicians} plays ${instruments}');
+    array.push('${musicians[i]} plays ${instruments[i]}');
   }
   
   return array;
