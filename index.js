ec2c9ef399d253b268b37399be8f8d042b1cc9cd
Liz Gunter <midwestunicorn@gmail.com>
2018-04-28 20:25:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 function theBeatlesPlay(musicians, instruments) {
-  var array = [];
+  const arr = [];
   for (let i = 0, l = musicians.length; i < l; i++) {
-    array.push('${musicians[i]} plays ${instruments[i]}');
+    arr.push('${musicians[i]} plays ${instruments[i]}');
   }
-  return array;
+  return arr;
 }
