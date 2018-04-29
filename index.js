59856c20637dab2cd50d30ec4370f1d526f2036a
Liz Gunter <midwestunicorn@gmail.com>
2018-04-29 00:15:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 
-const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 function theBeatlesPlay(musicians, instruments) {
-  const array = [];
+  var array = [];
   
   for (let i = 0, l = musicians.length; i < l; i++) {
     array.push('${musicians[i]} plays ${instruments[i]}');
@@ -12,14 +12,14 @@ function theBeatlesPlay(musicians, instruments) {
   return array;
 }
 
-const facts = [
+var facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"];
 
 function johnLennonFacts(facts) {
-  const shoutedFacts = [];
+  var shoutedFacts = [];
   
   let i = 0;
   while (i < facts.length) {
@@ -31,12 +31,12 @@ function johnLennonFacts(facts) {
 }
 
 function iLoveTheBeatles(n) {
-  const arr = [];
+  var array = [];
   
   do {
-    arr.push("I love the Beatles!");
+    array.push("I love the Beatles!");
     n++;
   } while (n < 15);
   
-  return arr;
+  return array;
 }
