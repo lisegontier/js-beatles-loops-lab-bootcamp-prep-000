29947c5d186f1f52c4218232377bcc2612e81d8d
Liz Gunter <midwestunicorn@gmail.com>
2018-04-29 00:19:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-
-var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
@@ -12,11 +9,6 @@ function theBeatlesPlay(musicians, instruments) {
   return array;
 }
 
-var facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"];
 
 function johnLennonFacts(facts) {
   var shoutedFacts = [];
